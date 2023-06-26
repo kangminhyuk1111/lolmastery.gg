fc76a3dc29fc6037e05b01290094696ea187de16
kangminhyuk1111 <rkdglqkr12@naver.com>
2023-06-26 16:16:16 +0900

    챔피언사진 다운로드 및 기능추가 mui연결 sass추가 nodejs 서버 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-import './App.css';
+import './App.scss';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { Button, TextField } from '@mui/material';
 
 function App() {
   const [summonerPid, setSummonerPid] = useState<any[]>([]);
   const [summonerName, setSummonerName] = useState();
-  const [championData, setChampionData] = useState<any[]>();
   const [champKey, setChampKey] = useState();
 
   const getSummonerInfo = (summonerName: string | undefined): void => {
@@ -13,7 +20,6 @@ function App() {
       .then((res) => {
         axios.get(`https://kr.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-puuid/${res.data.puuid}?api_key=${process.env.REACT_APP_RIOT_API_KEY}`).then((res) => {
           setSummonerPid(res.data);
-          console.log(res);
         })
       })
       .catch((err) => {
@@ -22,57 +28,69 @@ function App() {
         }
       });
   }
-  const versionChampId = () => {
+  const versionChampId = (): void => {
     axios.get('https://ddragon.leagueoflegends.com/cdn/13.6.1/data/ko_KR/champion.json').then((res) => {
-      setChampionData(res.data.data)
-      let champions = Object.values(res.data.data);
-      console.log(champions)
+
+      let champions: any[] = Object.values(res.data.data);
       let newObj: any = {};
       champions.map((data: any) => {
         let keys = data.key;
         let names = data.name;
-        newObj[keys] = { names: names, imgs: data.image.full };
+        newObj[keys] = { names: names, img: data.image.full };
       })
       setChampKey(newObj);
     })
   }
 
-  const downloadImgs = () => {
-    const savePath = `${process.env.PUBLIC_URL}/champImg`;
-  }
-
-  const inputChange = (e: any) => {
+  const inputChange = (e: any): void => {
     setSummonerName(e.target.value);
   }
 
   useEffect(() => {
-    downloadImgs();
     versionChampId();
+
   }, [])
 
   return (
     <div className="App">
-      <div className='summoner-name'>
-        <label>소환사 이름</label>
-        <input type='text' id='summonerName' onChange={inputChange} />
-        <button onClick={() => getSummonerInfo(summonerName)}>검색</button>
+      <div className='main-wrapper'>
+        <img src={`${process.env.PUBLIC_URL}/championImgs/rioticon.png`} />
+        <h1>LOL Champion Mastery</h1>
+        <h2>당신의 리그오브레전드 챔피언 숙련도를 검색하세요</h2>
+        <div className='summoner-name'>
+          <TextField label="소환사이름" variant="outlined" type='text' id='summonerName' onChange={inputChange} />
+          <Button variant="contained" onClick={() => getSummonerInfo(summonerName)}>검색</Button>
+        </div>
       </div>
-      {summonerPid ? summonerPid.map((data: any, idx: any) => {
-        console.log(champKey ? champKey[1]['names'] : null)
-        return (
-          <>
-            <ul>
-              <li>{champKey ? champKey[data.championId]['imgs'] : null}</li>
-              <li>
-                {champKey ? champKey[data.championId]['names'] : null}
-              </li>
-              <li>
-                {data.championPoints}
-              </li>
-            </ul>
-          </>
-        )
-      }) : '!'}
+      <TableContainer component={Paper} id='tb-container'>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>챔피언(사진)</TableCell>
+              <TableCell align="right">챔피언(이름)</TableCell>
+              <TableCell align="right">숙련도 레벨</TableCell>
+              <TableCell align="right">숙련도 점수</TableCell>
+              <TableCell align="right">최근 플레이</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {summonerPid.map((data: any, idx: number) => (
+              <TableRow
+                key={idx}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {champKey ? <img src={`${process.env.PUBLIC_URL}/championImgs/${champKey[data.championId]['img']}`} alt={`${champKey[data.championId]['img']}`} /> : null}
+                </TableCell>
+                <TableCell align="right">{champKey ? champKey[data.championId]['names'] : null}</TableCell>
+                <TableCell align="right">{data.championLevel}</TableCell>
+                <TableCell align="right">{data.championPoints}</TableCell>
+                <TableCell align="right">{data.protein}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 }
