c37aae41a5cdbed0c075a200b6d87d1e2c5e9473
kangminhyuk1111 <rkdglqkr12@naver.com>
2023-10-21 02:24:13 +0900

    FEAT: region selectbox 값 추가 및 map으로 실제 api 값으로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,6 @@ import Loading from "./component/Loading";
 import {BrowserRouter, Route, Routes, Navigate} from "react-router-dom";
 import MasteryPage from "./pages/MasteryPage";
 
-interface RegionList {
-    kr: string,
-    euw1: string,
-    jp1: string,
-    eun1: string,
-    br1: string,
-    la1: string,
-    la2: string,
-    th2: string,
-    th1: string,
-}
-
 function App() {
     return (
         <>
