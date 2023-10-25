4abf341977a0f7bf1c4673ca9c31946b4c77278c
kangminhyuk1111 <rkdglqkr12@naver.com>
2023-10-25 16:59:26 +0900

    FEAT: board - comment로 변경
    상세페이지에서 board - comment 조회 가능하여, 댓글작업 가능

--- src/pages/CommunityPage.tsx
+++ src/pages/CommunityPage.tsx
@@ -1,22 +1,29 @@
 import React, {useEffect, useState} from 'react';
-import { db } from '../firebase/firebase'
-import { collection, getDocs, addDoc, doc, updateDoc, deleteDoc } from 'firebase/firestore'
+import {db} from '../firebase/firebase'
+import {collection, getDocs, addDoc, doc, updateDoc, deleteDoc, query, where} from 'firebase/firestore'
 import '../css/community.scss'
 import Loding from "../component/Loading";
 import {Link} from "react-router-dom";
 
 // doc.id, doc.data()
 
-type setBoardData = {
-    title?: string;
-    content?: string;
-    writer?: string;
-    writeDate?: {seconds:number|undefined,nanoseconds:number|undefined}
-    comment?: [];
+type setBoardDataTypes = {
+    boardData : {
+        title?: string;
+        content?: string;
+        writer?: string;
+        category?: string;
+        writeDate?: { seconds: number | undefined, nanoseconds: number | undefined }
+    },
+    commentData? : {
+        commentContent?:string;
+        commentWriter?:string;
+        commentDate?:string;
+    }
 }
 
-const timeForToday = (value:number|undefined): string|undefined => {
-    if(value !== undefined){
+const timeForToday = (value: number | undefined): string | undefined => {
+    if (value !== undefined) {
         const today = new Date();
         const val = value * 1000
         const timeValue = new Date(val);
@@ -42,53 +49,46 @@ const timeForToday = (value:number|undefined): string|undefined => {
 }
 
 // firebase db 컬렉션 가져옴
-const boardCollectionRef = collection(db,'notice-board')
+const boardCollectionRef = collection(db, 'notice-board')
 
 // create board data
 const createFirebaseBoardDb = async () => {
-    await addDoc(boardCollectionRef, {title:"hello",content:"hello1"})
+    await addDoc(boardCollectionRef, {title: "hello", content: "hello1"})
 }
 
 // update board data
-const updateFirebaseBoardDb = async (collectionId:string,data:setBoardData) => {
-    const selectDoc = doc(db,"notice-board",collectionId)
-    const newBoardData = {title:data.title,content:data.content}
+const updateFirebaseBoardDb = async (collectionId: string, data: setBoardDataTypes) => {
+    const selectDoc = doc(db, "notice-board", collectionId)
+    const newBoardData = {title: data.boardData.title, content: data.boardData.content}
     await updateDoc(selectDoc, newBoardData)
 }
 
 // delect board data
-const deleteFirebaseBoardDb = async (collectionId:string) => {
-    const selectDoc = doc(db,"notice-board",collectionId)
+const deleteFirebaseBoardDb = async (collectionId: string) => {
+    const selectDoc = doc(db, "notice-board", collectionId)
     await deleteDoc(selectDoc)
 }
 
 const CommunityPage: React.FC = () => {
-    const [boardData, setBoardData] = useState<setBoardData[]>([])
+    const [boardData, setBoardData] = useState<any[]>([])
 
     useEffect(() => {
         // firebase db collection 조회
-        const getFirebaseBoardDb = async () => {
-            const boardData = await getDocs(boardCollectionRef)
+        const getFirebaseBoardDb = async (): Promise<void> => {
+            const boardData = await getDocs(boardCollectionRef);
 
-            let mapData = boardData.docs.map((doc) => {
-                return doc.data()
-            })
-
-            mapData = mapData.sort((a,b) => {
-                const aSeconds = a.writeDate?.seconds ?? 0;
-                const bSeconds = b.writeDate?.seconds ?? 0;
-
-                return bSeconds- aSeconds;
-            })
-
-            if (typeof mapData === 'object'){
-                setBoardData(mapData)
-            }
+            let mapData = await Promise.all(boardData.docs.map(async (doc, idx) => {
+                const commentDataSnapshot = await getDocs(query(collection(db, `/notice-board/${idx + 1}/comment`)));
+                const comments:object[] = [];
+                commentDataSnapshot.forEach((doc) => {
+                    comments.push(doc.data());
+                });
+                return { boardData: doc.data(), commentData: comments };
+            }));
+            setBoardData(mapData)
         }
         getFirebaseBoardDb()
     }, []);
-
-    // {item.content} {item.comment?.length !== 0 ? item.comment?.length : null}
 
     return (
         <div className='App'>
@@ -98,22 +98,22 @@ const CommunityPage: React.FC = () => {
                 <h2>자유롭게 대화하고 의견을 나눠주세요.</h2>
                 <div className={'community-board-div'}>
                     {boardData ? boardData.map((item, idx) => {
-                        return(
-                            <Link to={`/community/boardDetail/${idx+1}`} className={'board-list'}>
+                        return (
+                            <Link to={`/community/boardDetail/${idx + 1}`} className={'board-list'}>
                                 <div className='board-item list' key={idx}>
                                     <div className='board-item-1'/>
                                     <div className='board-item-2'>
-                                        <div className='board-item-2-top'>아 근데 이부분은 좀..<span className='comment-leng'>[17]</span></div>
+                                        <div className='board-item-2-top'>{item.boardData.title}<span className='comment-leng'>{item.commentData.length !== 0 ? `[${item.commentData.length}]` : null}</span></div>
                                         <div className='board-item-2-bottom'>
-                                            <p className='write-category'>유머</p>
-                                            <p className='write-date'>{timeForToday(item.writeDate?.seconds)}</p>
-                                            <p className='write-writer'>버거형</p>
+                                            <p className='write-category'>category</p>
+                                            <p className='write-date'>{timeForToday(item.boardData.writeDate?.seconds)}</p>
+                                            <p className='write-writer'>{item.boardData.writer}</p>
                                         </div>
                                     </div>
                                 </div>
                             </Link>
                         )
-                    }) : <Loding />}
+                    }) : <Loding/>}
                 </div>
             </div>
         </div>
