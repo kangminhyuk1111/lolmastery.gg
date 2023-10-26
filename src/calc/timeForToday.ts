e073028353437adcba9d97baf7fb918aafe82f6a
kangminhyuk1111 <rkdglqkr12@naver.com>
2023-10-26 09:12:52 +0900

    FEAT: timeForToday.ts 로 시간 변경 함수 이동

---
+++ src/calc/timeForToday.ts
@@ -0,0 +1,25 @@
+export const timeForToday = (value: number | undefined): string | undefined => {
+    if (value !== undefined) {
+        const today = new Date();
+        const val = value * 1000
+        const timeValue = new Date(val);
+
+        const betweenTime = Math.floor((today.getTime() - timeValue.getTime()) / 1000 / 60);
+        if (betweenTime < 1) return '방금전';
+        if (betweenTime < 60) {
+            return `${betweenTime}분전`;
+        }
+
+        const betweenTimeHour = Math.floor(betweenTime / 60);
+        if (betweenTimeHour < 24) {
+            return `${betweenTimeHour}시간전`;
+        }
+
+        const betweenTimeDay = Math.floor(betweenTime / 60 / 24);
+        if (betweenTimeDay < 365) {
+            return `${betweenTimeDay}일전`;
+        }
+
+        return `${Math.floor(betweenTimeDay / 365)}년전`;
+    }
+}
